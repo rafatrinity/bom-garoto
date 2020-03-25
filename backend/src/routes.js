0d7112ff932a60542081beb81c4e01632b50d09f
Rafael Pereira Da Trindade <rtrindade_accenture@timbrasil.com.br>
2020-03-25 16:01:55 -0300

    insert ongs ok

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,18 @@
-const express = require("express")
+const express = require("express");
+const crypto = require("crypto");
+const connection = require("./database/connection");
 const routes = express.Router();
 
-routes.get('/users/:id',(request, response) => {
-    const params = request.query;
-    id = request.params;
-    console.log(params, id);
-    
-    return response.send("hello world");
-})
+routes.get("/ongs", async (request, response) => {
+  const ongs = await connection("ongs").select("*");
+  return response.json(ongs);
+});
+
+routes.post("/ongs", async (request, response) => {
+  const { name, email, cel, city, uf } = request.body;
+  const id = crypto.randomBytes(4).toString("HEX");
+  await connection("ongs").insert({ id, name, email, cel, city, uf });
+  return response.json({ id });
+});
 
 module.exports = routes;
