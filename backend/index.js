5e622ad17ea82463668f8f5b8bce513847d47228
Rafael Pereira Da Trindade <rtrindade_accenture@timbrasil.com.br>
2020-03-24 23:40:23 -0300

    params ok

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const app = express();
+app.use(express.json());
 
-app.get('/',(request, response) => {
+app.get('/users/:id',(request, response) => {
+    const params = request.query;
+    id = request.params;
+    console.log(params, id);
+    
     return response.send("hello world");
 })
 
