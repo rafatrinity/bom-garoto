f2c0c9dbfc498e493eebdf06c698d8eb40f593a7
Rafael Pereira Da Trindade <rtrindade_accenture@timbrasil.com.br>
2020-03-26 14:12:26 -0300

    profi e sessions add

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const ongController = require("./controllers/ongController");
 const incidentController = require("./controllers/incidentController");
+const profileController = require("./controllers/profileController");
+const sessionController = require("./controllers/sessionController");
 const routes = express.Router();
 //ongs
 routes.get("/ongs", ongController.selectAll);
@@ -10,4 +12,9 @@ routes.post("/ongs", ongController.create);
 routes.get("/incidents", incidentController.selectAll);
 routes.post("/incidents", incidentController.create);
 routes.delete("/incidents/:id", incidentController.delete);
+//profile
+routes.get("/profile", profileController.listOngIncidents);
+//session
+routes.post("/login", sessionController.create);
+
 module.exports = routes;
