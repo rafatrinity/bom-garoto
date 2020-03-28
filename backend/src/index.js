05dce3c24e89b4d4b88a96754db634e5b274760e
Rafael Pereira Da Trindade <rtrindade_accenture@timbrasil.com.br>
2020-03-28 17:02:35 -0300

    validações ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { errors } = require("celebrate");
 const routes = require("./routes");
 app.use(express.json());
 app.use(routes);
-
+app.use(errors());
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,18 +3,51 @@ const ongController = require("./controllers/ongController");
 const incidentController = require("./controllers/incidentController");
 const profileController = require("./controllers/profileController");
 const sessionController = require("./controllers/sessionController");
+const {
+    celebrate,
+    Segments,
+    Joi
+} = require("celebrate");
 const routes = express.Router();
+
 //ongs
 routes.get("/ongs", ongController.selectAll);
 routes.get("/ongs/:name", ongController.selectByName);
-routes.post("/ongs", ongController.create);
+routes.post("/ongs", celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        cel: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), ongController.create);
+
 //incidents
-routes.get("/incidents", incidentController.selectAll);
+routes.get("/incidents", celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), incidentController.selectAll);
 routes.post("/incidents", incidentController.create);
-routes.delete("/incidents/:id", incidentController.delete);
+routes.delete("/incidents/:id", celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), incidentController.delete);
+
 //profile
-routes.get("/profile", profileController.listOngIncidents);
+routes.get("/profile", celebrate({
+    [Segments.HEADERS]: Joi.object().keys({
+        authorization: Joi.string().required()
+    }).unknown()
+}) , profileController.listOngIncidents);
+
 //session
-routes.post("/login", sessionController.create);
+routes.post("/login", celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required().length(8)
+    })
+}), sessionController.create);
 
 module.exports = routes;
