255d1f4bac0578e419cf7a6a5c6dde128f0114dd
RAFAEL TRINDADE <rafaeltrindade649@gmail.com>
2020-07-17 16:24:15 -0300

    install swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,25 @@ const cors = require("cors");
 const { errors } = require("celebrate");
 const routes = require("./routes/restrict.routes");
 const freeRoutes = require("./routes/free.routes");
+const swaggerJs = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: 'Bom garoto 🐾',
+            description: 'O Bom garoto 🐾 é um projeto que visa a integração de clinicas veterinárias com ONGs e doadores. esse projeto faz parte da semana OmniStack, da rocketseat e com o apoio e incentivo do professor Antonio Podgorski da UniCarioca, que propôs esse projeto como APS (atividade prática supervisionada).',
+            contact: {
+                name: 'Rafael Trindade'
+            },
+            servers: ['http://localhost:3333/']
+        }
+    },
+    apis: ['app.js']
+}
+const swaggerDocs = swaggerJs(swaggerOptions);
 app.use(express.json());
 app.use(freeRoutes);
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(routes);
 app.use(errors());
 module.exports = app;
--- backend/src/routes/free.routes.js
+++ backend/src/routes/free.routes.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const sessionController = require("../controllers/sessionController");
-const { celebrate, Segments, Joi } = require("celebrate");
+const {
+  celebrate,
+  Segments,
+  Joi
+} = require("celebrate");
 const freeRoutes = express.Router();
 //session
-freeRoutes.post("/login",celebrate({
+/**
+ *  @swagger
+ * /login:
+ *      post:
+ *       description: Faz o login na aplicação e retorna um token
+ *       responses:
+ *           '201':
+ *               description: A successful response
+ */
+freeRoutes.post("/login", celebrate({
     [Segments.BODY]: Joi.object().keys({
       email: Joi.string()
         .required()
